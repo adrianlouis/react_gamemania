1d1348474b6fe70f96f73afc7809fd9971430b27
AdrianLouis <adrian.louis.003@live.com>
2022-09-30 15:44:59 -0300

    Correção de dimensões responsivas para Galaxy S20 Fe

--- src/Header.js
+++ src/Header.js
@@ -23,10 +23,10 @@ const Header = () => {
       <span className="headerLogo">GameMania</span>
 
 <div className="headerBtns">
-<Link to='/'><span><i className="fa-solid fa-house"/> {width <= 360 ? '' : 'Home'}</span></Link>
+<Link to='/'><span><i className="fa-solid fa-house"/> {width <= 412 ? '' : 'Home'}</span></Link>
         {!context.logado ? (<>
-            <Link to='logcad'><i className="fa-solid fa-user" /> {width <= 360 ? '' : 'Entrar'}</Link>
-            <Link to='register'><i className="fa-solid fa-user-pen" /> {width <= 360 ? '' : 'Registrar'}</Link>   
+            <Link to='logcad'><i className="fa-solid fa-user" /> {width <= 412 ? '' : 'Entrar'}</Link>
+            <Link to='register'><i className="fa-solid fa-user-pen" /> {width <= 412 ? '' : 'Registrar'}</Link>   
         </>
         ) : (
           <>
@@ -37,7 +37,7 @@ const Header = () => {
         )}
         <Link to='cart'>
             <span style={{ cursor: "pointer", position: 'relative' }}>
-              <i className="fa-solid fa-cart-shopping" /> {width <= 360 ? '' : 'Carrinho'} 
+              <i className="fa-solid fa-cart-shopping" /> {width <= 412 ? '' : 'Carrinho'} 
             {numeroPedidos.wishlist.length !== 0 && <div className="headerCartNumber">{(numeroPedidos.wishlist).length}</div>}
             </span>
             </Link>
